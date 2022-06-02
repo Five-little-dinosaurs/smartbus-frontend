bee7fb91fe9280de50a8eb7b3468d1dcaed50fe6
1010353663 <1010353663@qq.com>
2022-06-02 21:37:12 +0800

    test

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
+  displayName: 'Test版本',
   email: 'demo@minimals.cc',
   photoURL: '/static/mock-images/avatars/avatar_default.jpg',
 };
