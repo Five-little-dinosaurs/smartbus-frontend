ed42e2436a900f8dbc129e4cbc0fa87140c26e9c
1010353663 <1010353663@qq.com>
2022-08-01 22:24:11 +0800

    update

--- src/pages/Echarts.js
+++ src/pages/Echarts.js
@@ -205,7 +205,7 @@ class Echarts extends Component {
     }
 
     render() {
-        return <div id="main" style={{ position: 'relative', width: 720, height: 500 }} />;
+        return <div id="main" style={{ position: 'relative', width: 720, height: 480 }} />;
     }
 }
 
